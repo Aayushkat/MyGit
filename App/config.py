114dfa694378356bffb60b0044c24bed2e6b0367
Aayush <aayushkatoch5577@gmail.com>
2026-08-12 22:25:26 +0530

    chore: Configured the env variables

---
+++ App/config.py
@@ -0,0 +1,8 @@
+#filename=config.py
+from pydantic_settings import BaseSettings,SettingsConfigDict
+
+class Settings(BaseSettings):
+    github_token:str | None=None
+    model_config = SettingsConfigDict(env_file=".env",extra="ignore")#'env_file' is the file path to load the enviroment file from
+                                                                    # and the extra is to handling(in this case ignore) extra variables 
+settings=Settings() 
